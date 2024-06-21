71034a8c580ae50a498c740744f7d26968867ae5
MicToProG <dylcr@Dylans_PC>
2024-06-20 22:29:37 -0500

    What

--- src/main/java/net/mcreator/nerdymcuniverses/NerdyMcuniversesMod.java
+++ src/main/java/net/mcreator/nerdymcuniverses/NerdyMcuniversesMod.java
@@ -17,9 +17,6 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.FriendlyByteBuf;
 
-import net.mcreator.nerdymcuniverses.init.NerdyMcuniversesModItems;
-import net.mcreator.nerdymcuniverses.init.NerdyMcuniversesModBlocks;
-
 import java.util.function.Supplier;
 import java.util.function.Function;
 import java.util.function.BiConsumer;
@@ -39,10 +36,6 @@ public class NerdyMcuniversesMod {
 		// End of user code block mod constructor
 		MinecraftForge.EVENT_BUS.register(this);
 		IEventBus bus = FMLJavaModLoadingContext.get().getModEventBus();
-
-		NerdyMcuniversesModBlocks.REGISTRY.register(bus);
-
-		NerdyMcuniversesModItems.REGISTRY.register(bus);
 
 		// Start of user code block mod init
 		// End of user code block mod init
